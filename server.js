9668d844c5489ff5abd389bf25698d3f9826a9f1
AnshulAnand <anandanshul001@gmail.com>
2022-12-06 16:08:35 +0530

    Added basic error handling

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const methodOverride = require('method-override');
 const campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas');
 const ejsMate = require('ejs-mate');
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -15,6 +17,15 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else { next() };
+};
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -25,8 +36,7 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if (!req.body.campground) throw new ExpressError('Invalid campground data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
   const camp = new campground(req.body.campground);
   await camp.save();
   res.redirect(`/campgrounds/${camp._id}`);
@@ -46,7 +56,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   res.render('campgrounds/edit', {camp});
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.body;
   const camp = await campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
