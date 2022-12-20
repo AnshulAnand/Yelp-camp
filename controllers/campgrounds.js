5895ee392fc48d3ea6db4d818b62232a1863563e
AnshulAnand <anandanshul001@gmail.com>
2022-12-20 16:18:46 +0530

    Added edit campgrounds functionality

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -63,24 +63,23 @@ module.exports.renderEditForm = async (req, res) => {
 // update campground
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  console.log(req.body)
-  const camp = await campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  const imgs = req.files.map(file => ({
-    url: file.path,
-    filename: file.filename,
-  }));
-  camp.images.push(...imgs);
-  await camp.save();
+  const camp = await campground.findByIdAndUpdate(id, { ...req.body.campground });
+  if (req.files) {
+    const imgs = req.files.map(file => ({ url: file.path, filename: file.filename }));
+    camp.images.push(...imgs);
+  }
   if (req.body.deleteImages) {
     for (let filename of req.body.deleteImages) {
-      await cloudinary.uploader.destroy(filename);
+      // await cloudinary.uploader.destroy(filename);
+      for (let i = 0; i < camp.images.length; i++) {
+        if (camp.images[i].filename == filename) camp.images.splice(i, 1);
+      }
     }
-    camp.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}});
+    // camp.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}});
   }
+  await camp.save();
   req.flash('success', 'Successfully updated campground!');
-  res.redirect(`/campgrounds/${campground._id}`);
+  res.redirect(`/campgrounds/${camp._id}`);
 };
 
 // delete campground
